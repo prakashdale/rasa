fe98429c34f1130bde05ec750e0b8f6e6e830643
Tobias Wochinger <t.wochinger@rasa.com>
2021-08-17 15:51:19 +0200

    implement NLU train graph generation

--- rasa/engine/recipes/default_recipe.py
+++ rasa/engine/recipes/default_recipe.py
@@ -1,9 +1,53 @@
 from __future__ import annotations
 
-from typing import Dict, Text, Any, Tuple
+from typing import Dict, Text, Any, Tuple, Type, Optional
 
-from rasa.engine.graph import GraphSchema
+from rasa.engine.graph import GraphSchema, GraphComponent, SchemaNode
 from rasa.engine.recipes.recipe import Recipe
+
+from rasa.nlu.classifiers.classifier import IntentClassifier
+from rasa.nlu.classifiers.diet_classifier import DIETClassifier
+from rasa.nlu.classifiers.sklearn_intent_classifier import SklearnIntentClassifier
+from rasa.nlu.extractors.crf_entity_extractor import CRFEntityExtractor
+from rasa.nlu.extractors.extractor import EntityExtractor
+from rasa.nlu.extractors.regex_entity_extractor import RegexEntityExtractor
+from rasa.nlu.featurizers.dense_featurizer.convert_featurizer import ConveRTFeaturizer
+from rasa.nlu.featurizers.dense_featurizer.lm_featurizer import LanguageModelFeaturizer
+from rasa.nlu.featurizers.dense_featurizer.mitie_featurizer import MitieFeaturizer
+from rasa.nlu.featurizers.dense_featurizer.spacy_featurizer import SpacyFeaturizer
+from rasa.nlu.featurizers.featurizer import Featurizer
+from rasa.nlu.featurizers.sparse_featurizer.lexical_syntactic_featurizer import (
+    LexicalSyntacticFeaturizer,
+)
+from rasa.nlu.selectors.response_selector import ResponseSelector
+from rasa.nlu.tokenizers.tokenizer import Tokenizer
+
+# TODO: Remove once they are implemented
+class ProjectProvider(GraphComponent):
+    pass
+
+
+class SchemaValidator(GraphComponent):
+    pass
+
+
+class FinetuningValidator(GraphComponent):
+    pass
+
+
+class NLUTrainingDataProvider(GraphComponent):
+    pass
+
+
+def _create_train(
+    component_class: Type,
+    config: Dict[Text, Any],
+    idx: int,
+    previous_node: Text,
+    last_feature_node: Optional[Text],
+) -> Tuple[Dict[Text, SchemaNode], Text, Optional[Text]]:
+    # TODO: Idea for refactoring stuff
+    pass
 
 
 class DefaultV1Recipe(Recipe):
@@ -12,4 +56,136 @@ class DefaultV1Recipe(Recipe):
     def schemas_for_config(
         self, config: Dict, cli_parameters: Dict[Text, Any]
     ) -> Tuple[GraphSchema, GraphSchema]:
-        pass
+        nodes = {
+            "project_provider": SchemaNode(
+                needs={},
+                uses=ProjectProvider,
+                constructor_name="create",
+                fn="provide",
+                config={},
+                is_input=True,
+            ),
+            "schema_validator": SchemaNode(
+                needs={"importer": "project_provider"},
+                uses=SchemaValidator,
+                constructor_name="create",
+                fn="validate",
+                config={},
+            ),
+            "finetuning_validator": SchemaNode(
+                needs={"importer": "schema_validator"},
+                uses=FinetuningValidator,
+                constructor_name="create",
+                fn="validate",
+                config={},
+            ),
+            # This starts the NLU part of the graph
+            "nlu_training_data_provider": SchemaNode(
+                needs={"importer": "finetuning_validator"},
+                uses=NLUTrainingDataProvider,
+                constructor_name="create",
+                fn="provide",
+                config={},
+                # TODO: not always
+                is_target=True,
+                is_input=True,
+            ),
+        }
+
+        import rasa.nlu.registry
+
+        last_run_node = "nlu_training_data_provider"
+        last_feature_node = None
+
+        idx = 0
+        for item in config["pipeline"]:
+            component_name = item.pop("name")
+            component = rasa.nlu.registry.get_component_class(component_name)
+            if issubclass(component, Tokenizer):
+                node_name = f"run_{component.__name__}{idx}"
+                nodes[node_name] = SchemaNode(
+                    needs={"training_data": last_run_node},
+                    uses=component,
+                    constructor_name="create",
+                    fn="process_training_data",
+                    config=item,
+                )
+                last_run_node = node_name
+            elif issubclass(component, Featurizer):
+                pretrained_featurizer = [
+                    LexicalSyntacticFeaturizer,
+                    SpacyFeaturizer,
+                    MitieFeaturizer,
+                    LanguageModelFeaturizer,
+                    ConveRTFeaturizer,
+                ]
+                if component in pretrained_featurizer:
+                    node_name = f"run_{component.__name__}{idx}"
+                    nodes[node_name] = SchemaNode(
+                        needs={"training_data": last_run_node},
+                        uses=component,
+                        constructor_name="create",
+                        fn="process_training_data",
+                        config=item,
+                    )
+                    last_run_node = node_name
+                else:
+                    train_node_name = f"train_{component.__name__}{idx}"
+                    nodes[train_node_name] = SchemaNode(
+                        needs={"training_data": last_run_node},
+                        uses=component,
+                        constructor_name="create",
+                        fn="train",
+                        config=item,
+                        is_target=True,
+                    )
+
+                    node_name = f"run_{component.__name__}{idx}"
+                    nodes[node_name] = SchemaNode(
+                        needs={
+                            "training_data": last_run_node,
+                            "resource": train_node_name,
+                        },
+                        uses=component,
+                        constructor_name="load",
+                        fn="process_training_data",
+                        config=item,
+                    )
+                    last_run_node = last_feature_node = node_name
+            elif issubclass(component, IntentClassifier):
+                trainable_classifiers = [
+                    DIETClassifier,
+                    SklearnIntentClassifier,
+                    ResponseSelector,
+                ]
+                if component in trainable_classifiers:
+                    node_name = f"train_{component.__name__}{idx}"
+                    nodes[node_name] = SchemaNode(
+                        needs={"training_data": last_feature_node},
+                        uses=component,
+                        constructor_name="create",
+                        fn="train",
+                        config=item,
+                        is_target=True,
+                    )
+                else:
+                    # We don't need non trainable classifiers
+                    continue
+            elif issubclass(component, EntityExtractor):
+                trainable_extractors = [CRFEntityExtractor, RegexEntityExtractor]
+                if component in trainable_extractors:
+                    pass
+                else:
+                    node_name = f"run_{component.__name__}{idx}"
+                    nodes[node_name] = SchemaNode(
+                        needs={"training_data": last_run_node},
+                        uses=component,
+                        constructor_name="load",
+                        fn="process_training_data",
+                        config=item,
+                    )
+                    last_run_node = node_name
+
+            idx += 1
+
+        return GraphSchema(nodes), GraphSchema({})
--- tests/engine/recipes/test_default_recipe.py
+++ tests/engine/recipes/test_default_recipe.py
@@ -1,7 +1,220 @@
-from rasa.engine.recipes.default_recipe import DefaultV1Recipe
+import rasa.shared.utils.io
+from rasa.engine.graph import GraphSchema, SchemaNode
+from rasa.engine.recipes.default_recipe import (
+    DefaultV1Recipe,
+    ProjectProvider,
+    SchemaValidator,
+    FinetuningValidator,
+    NLUTrainingDataProvider,
+)
 from rasa.engine.recipes.recipe import Recipe
+from rasa.engine.storage.resource import Resource
+from rasa.nlu.classifiers.diet_classifier import DIETClassifier
+from rasa.nlu.extractors.entity_synonyms import EntitySynonymMapper
+from rasa.nlu.featurizers.sparse_featurizer.count_vectors_featurizer import (
+    CountVectorsFeaturizer,
+)
+from rasa.nlu.featurizers.sparse_featurizer.lexical_syntactic_featurizer import (
+    LexicalSyntacticFeaturizer,
+)
+from rasa.nlu.featurizers.sparse_featurizer.regex_featurizer import RegexFeaturizer
+from rasa.nlu.selectors.response_selector import ResponseSelector
+from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
 
 
 def test_recipe_for_name():
     recipe = Recipe.recipe_for_name("default.v1")
     assert isinstance(recipe, DefaultV1Recipe)
+
+
+def test_generate_train_graph():
+    expected_train_schema = GraphSchema(
+        {
+            "project_provider": SchemaNode(
+                needs={},
+                uses=ProjectProvider,
+                constructor_name="create",
+                fn="provide",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=True,
+                resource=None,
+            ),
+            "schema_validator": SchemaNode(
+                needs={"importer": "project_provider"},
+                uses=SchemaValidator,
+                constructor_name="create",
+                fn="validate",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "finetuning_validator": SchemaNode(
+                needs={"importer": "schema_validator"},
+                uses=FinetuningValidator,
+                constructor_name="create",
+                fn="validate",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "nlu_training_data_provider": SchemaNode(
+                needs={"importer": "finetuning_validator"},
+                uses=NLUTrainingDataProvider,
+                constructor_name="create",
+                fn="provide",
+                config={},
+                eager=False,
+                # TODO: not always
+                is_target=True,
+                is_input=True,
+                resource=None,
+            ),
+            "run_WhitespaceTokenizer0": SchemaNode(
+                needs={"training_data": "nlu_training_data_provider"},
+                uses=WhitespaceTokenizer,
+                constructor_name="create",
+                fn="process_training_data",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "train_RegexFeaturizer1": SchemaNode(
+                needs={"training_data": "run_WhitespaceTokenizer0"},
+                uses=RegexFeaturizer,
+                constructor_name="create",
+                fn="train",
+                config={},
+                eager=False,
+                is_target=True,
+                is_input=False,
+                resource=None,
+            ),
+            "run_RegexFeaturizer1": SchemaNode(
+                needs={
+                    "training_data": "run_WhitespaceTokenizer0",
+                    "resource": "train_RegexFeaturizer1",
+                },
+                uses=RegexFeaturizer,
+                constructor_name="load",
+                fn="process_training_data",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "run_LexicalSyntacticFeaturizer2": SchemaNode(
+                needs={"training_data": "run_RegexFeaturizer1"},
+                uses=LexicalSyntacticFeaturizer,
+                constructor_name="create",
+                fn="process_training_data",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "train_CountVectorsFeaturizer3": SchemaNode(
+                needs={"training_data": "run_LexicalSyntacticFeaturizer2"},
+                uses=CountVectorsFeaturizer,
+                constructor_name="create",
+                fn="train",
+                config={},
+                eager=False,
+                is_target=True,
+                is_input=False,
+                resource=None,
+            ),
+            "run_CountVectorsFeaturizer3": SchemaNode(
+                needs={
+                    "training_data": "run_LexicalSyntacticFeaturizer2",
+                    "resource": "train_CountVectorsFeaturizer3",
+                },
+                uses=CountVectorsFeaturizer,
+                constructor_name="load",
+                fn="process_training_data",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "train_CountVectorsFeaturizer4": SchemaNode(
+                needs={"training_data": "run_CountVectorsFeaturizer3"},
+                uses=CountVectorsFeaturizer,
+                constructor_name="create",
+                fn="train",
+                config={"analyzer": "char_wb", "min_ngram": 1, "max_ngram": 4},
+                eager=False,
+                is_target=True,
+                is_input=False,
+                resource=None,
+            ),
+            "run_CountVectorsFeaturizer4": SchemaNode(
+                needs={
+                    "training_data": "run_CountVectorsFeaturizer3",
+                    "resource": "train_CountVectorsFeaturizer4",
+                },
+                uses=CountVectorsFeaturizer,
+                constructor_name="load",
+                fn="process_training_data",
+                config={"analyzer": "char_wb", "min_ngram": 1, "max_ngram": 4},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "train_DIETClassifier5": SchemaNode(
+                needs={"training_data": "run_CountVectorsFeaturizer4",},
+                uses=DIETClassifier,
+                constructor_name="create",
+                fn="train",
+                config={"epochs": 100, "constrain_similarities": True},
+                eager=False,
+                is_target=True,
+                is_input=False,
+                resource=None,
+            ),
+            "run_EntitySynonymMapper6": SchemaNode(
+                needs={"training_data": "run_CountVectorsFeaturizer4",},
+                uses=EntitySynonymMapper,
+                constructor_name="load",
+                fn="process_training_data",
+                config={},
+                eager=False,
+                is_target=False,
+                is_input=False,
+                resource=None,
+            ),
+            "train_ResponseSelector7": SchemaNode(
+                needs={"training_data": "run_CountVectorsFeaturizer4",},
+                uses=ResponseSelector,
+                constructor_name="create",
+                fn="train",
+                config={"epochs": 100, "constrain_similarities": True},
+                eager=False,
+                is_target=True,
+                is_input=False,
+                resource=None,
+            ),
+        }
+    )
+
+    config = rasa.shared.utils.io.read_yaml_file(
+        "rasa/shared/importers/default_config.yml"
+    )
+    recipe = Recipe.recipe_for_name(DefaultV1Recipe.name)
+    train_schema, _ = recipe.schemas_for_config(config, {})
+
+    for node_name, node in expected_train_schema.nodes.items():
+        assert train_schema.nodes[node_name] == node
+
+    assert train_schema == expected_train_schema
